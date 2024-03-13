8741c6eb8633130a9e83b2f130dc1bfd6ff7e04f
dreamlabo <toddlabo@gmail.com>
2024-03-12 23:48:59 -0600

    Landing page/sub text redesign.

--- scripts/carsCaseStudy/carsCaseStudyScripts.js
+++ scripts/carsCaseStudy/carsCaseStudyScripts.js
@@ -47,37 +47,68 @@ document.documentElement.style.setProperty('--scroll-padding', navigationHeight 
 //     observer.observe(heading)
 // })
 
-const sections = document.querySelectorAll(".section-heading");
-// Create a new Intersection Observer instance for each section
-sections.forEach(function(section) {
-    var observer = new IntersectionObserver(function(entries) {
-      entries.forEach(function(entry) {
-        // If the top of the container is within 100px from the top or bottom of the viewport
-        if ((entry.boundingClientRect.top > 40 && entry.boundingClientRect.bottom < 180)) {
-          // Remove the 'active' class from all navigation items
-          navLinks.forEach(function(item) {
-            item.classList.remove('active');
-          });
-          
-          // Get the corresponding navigation item and add the 'active' class
-          var targetId = entry.target.id;
-          var correspondingNavItem = document.querySelector('a[href="#' + targetId + '"]');
-          if (correspondingNavItem) {
-            correspondingNavItem.classList.add('active');
-          }
+// Intersection observer for side navigation section indicator
+// const headingsToObserve = document.querySelectorAll(".section-top");
+// console.log(headingsToObserve)
+
+const headingsToObserve = document.querySelectorAll(".section-heading");
+
+var observer = new IntersectionObserver(function(entries) {
+    entries.forEach(function(entry) {
+      // If the section is intersecting with the viewport
+    //   console.log(entry, entry.boundingClientRect.top)
+     if ((entry.boundingClientRect.top > 40 && entry.boundingClientRect.bottom < 200)) {
+        console.log(entry, entry.boundingClientRect.top)
+        // Remove the 'active' class from all navigation items
+        navLinks.forEach(function(link) {
+          link.classList.remove('active');
+        });
+        
+        // Get the corresponding navigation item and add the 'active' class
+        const targetId = entry.target.id;
+        const correspondingNavItem = document.querySelector('a[href="#' + targetId + '"]');
+        if (correspondingNavItem) {
+          correspondingNavItem.classList.add('active');
         }
-      });
-    }, {rootMargin: '-100px 0px 0px 0px' }); // Set the top and bottom margin to -100px
+      }
+    });
+  }, { rootMargin: '-180px 0px 0px 0px' });
+
+headingsToObserve.forEach(heading => {
+    observer.observe(heading)
+})
+
+// const sections = document.querySelectorAll(".section-heading");
+// // Create a new Intersection Observer instance for each section
+// sections.forEach(function(section) {
+//     var observer = new IntersectionObserver(function(entries) {
+//       entries.forEach(function(entry) {
+//         // If the top of the container is within 100px from the top or bottom of the viewport
+//         if ((entry.boundingClientRect.top > 40 && entry.boundingClientRect.bottom < 200)) {
+//           // Remove the 'active' class from all navigation items
+//           navLinks.forEach(function(item) {
+//             item.classList.remove('active');
+//           });
+          
+//           // Get the corresponding navigation item and add the 'active' class
+//           var targetId = entry.target.id;
+//           var correspondingNavItem = document.querySelector('a[href="#' + targetId + '"]');
+//           if (correspondingNavItem) {
+//             correspondingNavItem.classList.add('active');
+//           }
+//         }
+//       });
+//     }, {rootMargin: '-100px 0px 0px 0px' }); // Set the top and bottom margin to -100px
   
-    // Observe the container
-    observer.observe(section);
-  });
+//     // Observe the container
+//     observer.observe(section);
+//   });
   
-  // Function to calculate the threshold based on the height of each section
-  function calculateThreshold(sectionHeight) {
-    // Calculate the threshold dynamically based on the height of the section
-    return Math.min(1, 100 / (sectionHeight/4));
-  }
+//   // Function to calculate the threshold based on the height of each section
+//   function calculateThreshold(sectionHeight) {
+//     // Calculate the threshold dynamically based on the height of the section
+//     return Math.min(1, 100 / (sectionHeight/4));
+//   }
 
 
 
