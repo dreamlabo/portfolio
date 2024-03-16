f646874f569d2ee363dd2ae82d6395003ab5af16
dreamlabo <toddlabo@gmail.com>
2024-03-16 16:34:14 -0600

    Added intro screen animation.

--- scripts/homePageScripts/homePage.js
+++ scripts/homePageScripts/homePage.js
@@ -16,3 +16,28 @@ const observer = new IntersectionObserver(
 aboutTextElements.forEach(element => {
     observer.observe(element);
 });
+
+
+const introScreen = document.getElementById("intro-screen")
+const topLetters = document.querySelectorAll(".top-logo-letters");
+const logoBottom = document.querySelector(".logo-bottom")
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    setTimeout(() => {
+            topLetters.forEach((letter, index) => {
+                setTimeout(() => {
+                letter.classList.add("active");
+            }, (index + 1) * 150)
+        })
+
+        setTimeout(() => {
+            logoBottom.classList.add("animate-up")
+        }, 800)
+
+        setTimeout(()=> {
+            introScreen.style.top = '-100vh'
+        }, 1600)
+
+    }, 2000)
+})
